c36f3e62f3117658a3cf71d675c16304b544bf3f
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-03-07 15:41:41 -0600

    Sesión 20 - 07/03/2025

--- 04 - Objetos Graficos/Dibujo.java
+++ 04 - Objetos Graficos/Dibujo.java
@@ -17,9 +17,25 @@ public class Dibujo extends Frame {
     }
     
     public void paint( Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.drawString("Saludos Programas!!!", 100, 100);
-
+        Graphics2D gc2d = (Graphics2D) g;
+        Color color = new Color(50, 26, 94);
+        gc2d.setColor(Color.BLACK);
+        gc2d.setBackground(Color.black);
+        gc2d.clearRect(0, 0, getWidth(), getHeight());
+        gc2d.setStroke(new BasicStroke(4.0f));
+        gc2d.setColor(Color.GRAY);
+        gc2d.fill( new Arc2D.Float(68, 68, 170, 179, 35, 280, Arc2D.PIE) );
+        gc2d.setPaint(new GradientPaint(145, 180, Color.YELLOW, 
+                                        50, 50, Color.LIGHT_GRAY, true));
+        gc2d.fill( new Arc2D.Float(70, 70, 175, 175, 35, 280, Arc2D.PIE) );
+        gc2d.fill( new Arc2D.Float(70, 70, 175, 175, 35, 200, Arc2D.PIE) );
+        gc2d.setColor(color);
+        gc2d.fill(new Ellipse2D.Float(140,110,18,18));
+        gc2d.setPaint(new GradientPaint(200, 100, new Color(232,214,19), 
+                                        230, 160, Color.YELLOW, true));
+        gc2d.fill(new Ellipse2D.Float(200, 145, 30, 30));
+        gc2d.fill(new Ellipse2D.Float(270, 145, 30, 30));
+        gc2d.fill(new Ellipse2D.Float(340, 145, 30, 30));
 
         /*Color color = new Color(255, 255, 0);
         Font f = new Font("Arial", Font.ITALIC + Font.BOLD, 18);
@@ -33,6 +49,7 @@ public class Dibujo extends Frame {
         g.setColor(Color.pink);
         g.fillOval(50, 150, 90, 90);
         g.fillArc(250, 100, 100, 50, 0, 180);
+
         Color color = new Color(50, 26, 94);
         g.setColor(Color.black);
         g.fillRect(0,0,getWidth(), getHeight());
