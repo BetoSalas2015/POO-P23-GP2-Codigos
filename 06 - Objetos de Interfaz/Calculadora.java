d29ff723c435253b6de481ba39c8e1459bbdbb3d
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-03-28 15:56:38 -0600

    Sesión 28 - 28/03/2025

---
+++ 06 - Objetos de Interfaz/Calculadora.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+
+public class Calculadora extends Frame {
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    private Button btnMas, btnMenos, btnMult, btnDiv, btnPunto, btnIgual, btnC;
+    private TextField txtDisplay;
+    private Panel pnlDisplay, pnlTeclado;
+
+    public Calculadora() {
+        super("Calculadora"); 
+
+        // Creación de Objetos
+        btn0 = new Button("0");         btn1 = new Button("1");
+        btn2 = new Button("2");         btn3 = new Button("3");
+        btn4 = new Button("4");         btn5 = new Button("5");
+        btn6 = new Button("6");         btn7 = new Button("7");
+        btn8 = new Button("8");         btn9 = new Button("9");
+        btnMas = new Button("+");       btnMenos = new Button("-");
+        btnMult = new Button("*");      btnDiv = new Button("/");
+        btnIgual = new Button("=");     btnPunto = new Button(".");
+        btnC = new Button("C");
+
+        txtDisplay = new TextField("0");
+
+        pnlDisplay = new Panel();
+        pnlTeclado = new Panel();
+
+        // Organizar los páneles
+        pnlTeclado.setLayout( new GridLayout(4, 4, 2, 2) );
+        pnlTeclado.add(btn7);
+        pnlTeclado.add(btn8);
+        pnlTeclado.add(btn9);
+        pnlTeclado.add(btnDiv);
+        pnlTeclado.add(btn4);
+        pnlTeclado.add(btn5);
+        pnlTeclado.add(btn6);
+        pnlTeclado.add(btnMult);
+        pnlTeclado.add(btn1);
+        pnlTeclado.add(btn2);
+        pnlTeclado.add(btn3);
+        pnlTeclado.add(btnMenos);
+        pnlTeclado.add(btnIgual);
+        pnlTeclado.add(btn0);
+        pnlTeclado.add(btnPunto);
+        pnlTeclado.add(btnMas);
+
+        pnlDisplay.setLayout(new BorderLayout());
+        pnlDisplay.add(txtDisplay,"Center");
+        pnlDisplay.add(btnC, "East");
+
+        add(pnlDisplay, "North");
+        add(pnlTeclado, "Center");
+
+    }
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.resize(300, 320);
+        calc.show();
+    }
+    
+}
